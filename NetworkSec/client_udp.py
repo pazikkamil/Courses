35e4c451c1d013e12fe4974bffc7d5dec3df1342
kamil <pazik.kamil@gmail.com>
2016-12-29 03:05:33 +0100

    +udp client

---
+++ NetworkSec/client_udp.py
@@ -0,0 +1,13 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+banner = "AAABBBCCC".encode()
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.sendto(banner, (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print(data)
