92f376c46e932fa866d1e54781451932e97c14f5
kamil <pazik.kamil@gmail.com>
2016-12-29 21:13:00 +0100

    nowy

--- NetworkSec/server_tcp.py
+++ NetworkSec/server_tcp.py
@@ -4,6 +4,7 @@ import threading
 bind_ip = "0.0.0.0"
 bind_port = 9999
 
+
 def handle_client(client_socket):
     """
     Fuction print all info sent from client.
@@ -18,7 +19,7 @@ def handle_client(client_socket):
     request = client_socket.recv(1024)
 
     print("[*] Received: %s" % request)
-    client_socket.send("ACK!")
+    client_socket.send("ACK!".encode())
 
     client_socket.close()
 
@@ -27,11 +28,16 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((bind_ip, bind_port))
 
+# listen max to 5 connections
 server.listen(5)
 
 print("[*] Listening on the port %s:%d" % (bind_ip, bind_port))
 
 while True:
+    # Main loop waiting for connection
+
+    # client is socket of connection
+    # addr is connection data
     client, addr = server.accept()
 
     print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
