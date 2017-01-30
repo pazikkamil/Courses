a2c12d4a2c63b85f165ce619851f42c0248d58c2
kamil <pazik.kamil@gmail.com>
2017-01-30 01:11:31 +0100

    modified imap_client

--- NetworkSec/imap_client.py
+++ NetworkSec/imap_client.py
@@ -14,28 +14,63 @@ PASSWORD = os.environ.get('GMAIL_PASSWD')
 ssl = True
 
 
-# def get_past_emails(self, folder, from_date=None, to_date=None, from_person=None, to_person=None):
-#    """Get past emails from/to given date/person"""
+def get_past_emails(folder="", from_date="", to_date="", from_person="", to_person=""):
+    """Get past emails from/to given date/person"""
+    global HOST
+    global USERNAME
+    global PASSWORD
+    server = IMAPClient(HOST, use_uid=True, ssl=ssl)
+    server.login(USERNAME, PASSWORD)
+    # IF wp.pl -> use_uid = False
+    # IF gmail -> use_uid = True
+    imap_query_start = u"("
+    imap_query_end = ")"
+    imap_query=""
 
-# IF wp.pl -> use_uid = False
-# IF gmail -> use_uid = True
-server = IMAPClient(HOST, use_uid=True, ssl=ssl)
-server.login(USERNAME, PASSWORD)
 
-folders_list = server.list_folders()
+    #folders_list = server.list_folders()
+    if folder is None:
+        folder = b'INBOX'
 
-#server.search(u'(HEADER TO "{}")'.format("alicja.karalus@instream.io")))
+    select_info = server.select_folder(folder)
 
-select_info = server.select_folder('INBOX')
-print('%d messages in INBOX' % select_info['EXISTS'])
+    if from_person:
+        imap_query += 'FROM {}'.format(from_person)
+    if to_person:
+        imap_query += ' TO {}'.format(to_person)
+    if from_date:
+        imap_query += ' SINCE {}'.format(from_date)
+    if to_date:
+        imap_query += ' BEFORE {}'.format(to_date)
 
-messages = server.search(['NOT', 'DELETED'])
-print("%d messages that aren't deleted" % len(messages))
+    if len(imap_query) > 0:
+        imap_query = "{}{}{}".format(imap_query_start, imap_query, imap_query_end)
+        messages = server.search(imap_query)
+    else:
+        messages = server.search()
 
-print()
-print("Messages:")
-response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
-for msgid, data in response.iteritems():
-    print('   ID %d: %d bytes, flags=%s' % (msgid,
-                                            data[b'RFC822.SIZE'],
-                                            data[b'FLAGS']))
+    # search_query = "{} {} {} {} {} {}".format(
+    #     imap_query_start,
+    #     imap_from,
+    #     imap_to,
+    #     imap_since,
+    #     imap_before,
+    #     imap_query_end
+    # )
+
+
+    #print('%d messages in INBOX' % select_info['EXISTS']) # or b'EXISTS'
+
+    # messages = server.search(['NOT', 'DELETED'])
+    # print("%d messages that aren't deleted" % len(messages))
+
+    print()
+    print("Messages:")
+    response = server.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
+    for msgid, data in response.items():
+    #for msgid, data in response.iteritems():
+        print('   ID %d: %d bytes, flags=%s' % (msgid,
+                                                data[b'RFC822.SIZE'],
+                                                data[b'FLAGS']))
+
+x = get_past_emails(b'INBOX',from_person='kamil.pazik@instream.io', from_date='01-JAN-2017')
